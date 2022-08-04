8eb27cb08b20d7b2688d8ebc8d727d5b6f27376d
Ben Benoit <benoit.ben.j@gmail.com>
2022-08-04 17:24:41 -0500

    refactored with new Game.js file. Added Wall objects

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
 "use strict";
 import { assertNotNull, assertInstancesOf, assertWithinRange, getNewElement, crash } from './lib/Util.js';
-// import { Vector2, Vec } from './lib/Vector2.js';
 import { Vector2, vector2 } from './lib/Vector2.js';
-import { Rect2 } from './lib/Rect2.js';
+import { Rect2, rect2 } from './lib/Rect2.js';
+import { Color, color } from './lib/Color.js';
 import { GameObj } from './lib/GameObj.js';
-import { Color } from './lib/Color.js';
+import { gameArea } from './lib/Game.js';
 
-const gameContainer = document.querySelector("game-container");
-const gameContainerStyle = getComputedStyle(gameContainer);
-const gameArea = vector2(parseInt(gameContainerStyle.width), parseInt(gameContainerStyle.height))
 const FPS = 60;
 const tickRate = (1.0 / FPS) * 1000;
 const GRAVITY = 1;
-const JUMP_FORCE = -13
+const JUMP_FORCE = -10;
 const tick = () => {applyGravity();}
 let physicsInterval;
 // clearInterval(physicsInterval);
 
-const playerGameObj = new GameObj(
-    {
-        rect2: new Rect2(Vector2.ZERO, new Vector2(16, 16)),
-        backgroundColor: Color.RED,
-    })
+const WALL_WIDTH = 16
+const WALL_SPEED = -20 // wall movement speed
+const HOLE_HEIGHT = 85 // height of gap between walls
 
-playerGameObj.addToGame();
+const playerGameObj = new GameObj({ rect2: rect2(Vector2.ZERO, vector2(16, 16)), backgroundColor: Color.WHITE })
+
+const getNewWall = () => {
+    return new GameObj(
+        { 
+            rect2: rect2(Vector2.ZERO, vector2(WALL_WIDTH, (gameArea.getY - HOLE_HEIGHT) / 2.0)),
+            backgroundColor: Color.RED,
+        });
+    }
+
+
+const topWall = getNewWall();
+const bottomWall = getNewWall();
+const walls = [topWall, bottomWall];
+
+[playerGameObj, topWall, bottomWall].forEach(obj => {obj.addToGame();})
+
+bottomWall.setY = bottomWall.getMaxY;
+walls.forEach(wall => {wall.setX = 40;})
 
 const applyGravity = () => {
     playerGameObj.offsetVelocity(vector2(0, GRAVITY));
     playerGameObj.offsetPos(playerGameObj.getVelocity);
 
-    const playerMaxY = gameArea.getY - playerGameObj.getHeight;
-
-    if (playerGameObj.getY > playerMaxY) {
+    if (playerGameObj.getY > playerGameObj.getMaxY) {
         playerGameObj.setVelocity = Vector2.ZERO;
-        playerGameObj.setY = playerMaxY;
+        playerGameObj.setY = playerGameObj.getMaxY;
     } else if (playerGameObj.getY < 0) {
         playerGameObj.setVelocity = Vector2.ZERO;
         playerGameObj.setY = 0;
@@ -42,7 +53,6 @@ const applyGravity = () => {
 
 window.addEventListener("keydown", (event) => {
     if (event.key == " " && !event.repeat){ // spacebar
-        console.log("pressed spacebar.");
         playerGameObj.setVelocity = vector2(0, JUMP_FORCE)
     }
 })
